fd2c349e04adc75bbdf14090202d2a15f390868b
nurull.e <chotimahnurul23@gmail.com>
2020-12-01 10:48:15 +0700

    user

---
+++ app/src/main/java/com/choco/finalprojectoop/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.choco.finalprojectoop
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+        val config = RealmConfiguration.Builder().name("myrealm.realm").build()
+        Realm.setDefaultConfiguration(config)
+    }
+}
