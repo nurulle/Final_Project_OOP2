2c7d0b70fe6a9d7cbffb664a8f3e134c024f8f10
nurull.e <chotimahnurul23@gmail.com>
2020-12-08 15:09:48 +0700

    tambahdata

---
+++ app/src/main/java/com/choco/finalprojectoop/DosenActivity.kt
@@ -0,0 +1,21 @@
+package com.choco.finalprojectoop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.choco.finalprojectoop.realm.UserChoco
+import com.choco.finalprojectoop.realm.UserChocoAdapter
+import io.realm.Realm
+
+class DosenActivity : AppCompatActivity() {
+
+    lateinit var userChocoAdapter: UserChocoAdapter
+    var ln = LinearLayoutManager(this)
+
+
+    lateinit var realm: Realm
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dosen)
+    }
+}
---
+++ app/src/main/java/com/choco/finalprojectoop/realm/UserChocoAdapter.kt
@@ -0,0 +1,4 @@
+package com.choco.finalprojectoop.realm
+
+class UserChocoAdapter {
+}
